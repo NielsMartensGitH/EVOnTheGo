b590fe457f613aedb353d52708e09b6d761b6242
Niels <niels_martens@live.nl>
2021-11-19 22:39:48 +0100

    bug fixed caused by previous code changes.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 /* ============ API VARIABLES ===================*/
 
-const key = "dnA4PR9uKOU3Ltk0V7Fb8A5t6vHnsguc";  // MKB8TegqvUZ7OxWFWLC5zRepju2cstNK 2nd key when limit fetches has reached
+const key = "MKB8TegqvUZ7OxWFWLC5zRepju2cstNK";  //dnA4PR9uKOU3Ltk0V7Fb8A5t6vHnsguc  2nd key when limit fetches has reached
 const category = "electric%20vehicle%20station";
 const url = "https://api.tomtom.com/search/2/categorySearch/" + category + ".json?key=" + key;
 const initialPlace = [5.305940, 50.842289]; // place map displays first when opening app
@@ -56,7 +56,7 @@ function submit(){
     removePreviousLayer();
     begin = document.getElementById("begin").value;
     einde = document.getElementById("einde").value;
-    connectorType = document.getElementById("connector").value;
+    // connectorType = document.getElementById("connector").value;
     kabel = document.getElementById("kabel").checked;
 /*
     console.log("Begin: "+begin);
