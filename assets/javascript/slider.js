92e124b129c99a8ea6490bd0a49d801da7a9fea7
Niels <niels_martens@live.nl>
2021-11-22 15:11:20 +0100

    added slider for kwh values

---
+++ assets/javascript/slider.js
@@ -0,0 +1,39 @@
+window.onload = function(){
+    slideOne();
+    slideTwo();
+}
+
+let sliderOne = document.getElementById("slider-1");
+let sliderTwo = document.getElementById("slider-2");
+let displayValOne = document.getElementById("range1");
+let displayValTwo = document.getElementById("range2");
+let minGap = 0;
+let sliderTrack = document.querySelector(".slider-track");
+let sliderMaxValue = document.getElementById("slider-1").max;
+
+console.log(sliderOne.value + "sliderone")
+console.log(displayValOne.textContent)
+console.log(sliderTwo.value + "slidertwo")
+
+function slideOne(){
+    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+        sliderOne.value = parseInt(sliderTwo.value) - minGap;
+        console.log("slideOne function")
+        console.log(sliderOne.value)
+    }
+    displayValOne.textContent = sliderOne.value;
+    fillColor();
+}
+function slideTwo(){
+    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+        sliderTwo.value = parseInt(sliderOne.value) + minGap;
+        console.log("SlideTwo function")
+    }
+    displayValTwo.textContent = sliderTwo.value;
+    fillColor();
+}
+function fillColor(){
+    percent1 = (sliderOne.value / sliderMaxValue) * 100;
+    percent2 = (sliderTwo.value / sliderMaxValue) * 100;
+    sliderTrack.style.background = `linear-gradient(to right, #FFFFFF ${percent1}% , #0D1E50 ${percent1}% , #0D1E50 ${percent2}%, #FFFFFF ${percent2}%)`;
+}
